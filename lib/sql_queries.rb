987b759d51488882536bc9f3dbd48caee6836bf1
Kelsey Fecho <kfecho30@gmail.com>
2018-03-29 15:46:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, SUM(pledges.amount) FROM projects
   JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY project.category
+  GROUP BY projects.category
   WHERE projects.category = 'music';"
 end
 
